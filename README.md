cedebfa35b6fff0f7960719ebba330a503eb1030
edi-sipka@gmail.com <edisipka@gmail.com>
2022-08-02 07:59:20 +0100

    Add javascript remove

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # Awesome-books
-Awesome books
+
+Javascript project that is adding and removing book list.
+
+## Built With
+
+- HTML
+- CSS
+- Javascript
+  To get a local copy up and running follow these simple example steps.
+
+## Authors
+
+**Jonah Wambua**
+
+- GitHub: [@githubhandle](https://github.com/DJ-MrJay)
+- Twitter: [@twitterhandle](https://twitter.com/jonah_wambua)
+- LinkedIn: [LinkedIn](https://www.linkedin.com/in/mr-jay/)
+
+**Edi Sipka**
+
+- GitHub: [@Edi-Sipka](https://github.com/edi-sipka)
+- Twitter: [@sipka_edii](https://twitter.com/sipka_edii)
+- LinkedIn: [Edi Sipka](https://www.linkedin.com/in/edi-%C5%A1ipka-5b681b202/)
+
+##  Contributing
+
+Contributions, issues, and feature requests are welcome! Feel free to check the [issues page](../../issues/).
+
+## Show your support
+
+Give a :star:️ if you like this project!
+
+## Acknowledgments
+
+- Hat tip to anyone whose code was used
+- Inspiration
+- etc
+
+## :memo: License
+
+This project is [MIT](./MIT.md) licensed.
--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const addButton = document.querySelector('.button');
+const form = document.querySelector('.input-form');
 const author = document.querySelector('.author');
 const title = document.querySelector('.title');
 const list = document.querySelector('.list');
-
-const collection = [];
+const collection = [] || JSON.parse(localStorage.getItem('form'));
 
 addButton.addEventListener('click', (e) => {
   e.preventDefault();
@@ -11,8 +11,10 @@ addButton.addEventListener('click', (e) => {
     const book = {
       title: title.value,
       author: author.value,
+      id: Math.round(Date.now()),
     };
     collection.push(book);
+    localStorage.setItem('form', JSON.stringify(collection));
     addingBooks();
     author.value = '';
     title.value = '';
@@ -30,10 +32,27 @@ const addingBooks = () => {
     const removeButton = document.createElement('button');
     removeButton.classList.add('remove');
     removeButton.textContent = 'Remove';
+    removeButton.setAttribute('id', item.id);
 
     bookDiv.appendChild(paragraph);
     bookDiv.appendChild(removeButton);
     list.appendChild(bookDiv);
   });
 };
+
 addingBooks();
+
+list.addEventListener('click', (e) => {
+  if (e.target.classList.contains('remove')) {
+    const targetid = parseInt(e.target.getAttribute('id'), 10);
+    removeBook(targetid);
+  }
+});
+
+function removeBook(targetId) {
+  const newArr = collection.filter((item) => item.id !== targetId);
+  collection.length = 0;
+  collection.push(...newArr);
+  localStorage.setItem('form', JSON.stringify(collection));
+  addingBooks();
+}
