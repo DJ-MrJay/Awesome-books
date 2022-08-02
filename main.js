75061bfa3e35aa37be5975fd09f77e0c7ba11acf
DJ-MrJay <97578860+DJ-MrJay@users.noreply.github.com>
2022-08-02 11:20:26 +0300

    Fix linter errors

--- main.js
+++ main.js
@@ -1,10 +1,29 @@
 const addButton = document.querySelector('.button');
-const form = document.querySelector('.input-form');
 const author = document.querySelector('.author');
 const title = document.querySelector('.title');
 const list = document.querySelector('.list');
 const collection = [] || JSON.parse(localStorage.getItem('form'));
+const addingBooks = () => {
+  list.innerHTML = '';
+  collection.forEach((item) => {
+    const bookDiv = document.createElement('div');
+    bookDiv.classList.add('book-div');
+    const paragraph = document.createElement('p');
+    paragraph.classList.add('p1');
+    paragraph.textContent = `${item.title} written by ${item.author}`;
+    const removeButton = document.createElement('button');
+    removeButton.classList.add('remove');
+    removeButton.textContent = 'Remove';
+    removeButton.setAttribute('id', item.id);
+    const divider = document.createElement('hr');
+    bookDiv.appendChild(paragraph);
+    bookDiv.appendChild(removeButton);
+    list.appendChild(bookDiv);
+    bookDiv.appendChild(divider);
+  });
+};
 
+addingBooks();
 addButton.addEventListener('click', (e) => {
   e.preventDefault();
   if (author.value && title.value) {
@@ -21,35 +40,6 @@ addButton.addEventListener('click', (e) => {
   }
 });
 
-const addingBooks = () => {
-  list.innerHTML = '';
-  collection.forEach((item) => {
-    const bookDiv = document.createElement('div');
-    bookDiv.classList.add('book-div');
-    const paragraph = document.createElement('p');
-    paragraph.classList.add('p1');
-    paragraph.textContent = `${item.title} written by ${item.author}`;
-    const removeButton = document.createElement('button');
-    removeButton.classList.add('remove');
-    removeButton.textContent = 'Remove';
-    removeButton.setAttribute('id', item.id);
-    const divider = document.createElement ('hr');
-    bookDiv.appendChild(paragraph);
-    bookDiv.appendChild(removeButton);
-    list.appendChild(bookDiv);
-    bookDiv.appendChild(divider);
-  });
-};
-
-addingBooks();
-
-list.addEventListener('click', (e) => {
-  if (e.target.classList.contains('remove')) {
-    const targetid = parseInt(e.target.getAttribute('id'), 10);
-    removeBook(targetid);
-  }
-});
-
 function removeBook(targetId) {
   const newArr = collection.filter((item) => item.id !== targetId);
   collection.length = 0;
@@ -57,3 +47,10 @@ function removeBook(targetId) {
   localStorage.setItem('form', JSON.stringify(collection));
   addingBooks();
 }
+
+list.addEventListener('click', (e) => {
+  if (e.target.classList.contains('remove')) {
+    const targetid = parseInt(e.target.getAttribute('id'), 10);
+    removeBook(targetid);
+  }
+});
