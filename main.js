4a3313c20c5e3656df51570b44041348287931b8
DJ-MrJay <97578860+DJ-MrJay@users.noreply.github.com>
2022-08-03 12:48:45 +0300

    Fix linter errors

---
+++ main.js
@@ -0,0 +1,56 @@
+const addButton = document.querySelector('.button');
+const author = document.querySelector('.author');
+const title = document.querySelector('.title');
+const list = document.querySelector('.list');
+const collection = [] || JSON.parse(localStorage.getItem('form'));
+const addingBooks = () => {
+  list.innerHTML = '';
+  collection.forEach((item) => {
+    const bookDiv = document.createElement('div');
+    bookDiv.classList.add('book-div');
+    const paragraph = document.createElement('p');
+    paragraph.classList.add('p1');
+    paragraph.textContent = `${item.title} written by ${item.author}`;
+    const removeButton = document.createElement('button');
+    removeButton.classList.add('remove');
+    removeButton.textContent = 'Remove';
+    removeButton.setAttribute('id', item.id);
+    const divider = document.createElement('hr');
+    bookDiv.appendChild(paragraph);
+    bookDiv.appendChild(removeButton);
+    list.appendChild(bookDiv);
+    bookDiv.appendChild(divider);
+  });
+};
+
+addingBooks();
+addButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  if (author.value && title.value) {
+    const book = {
+      title: title.value,
+      author: author.value,
+      id: Math.round(Date.now()),
+    };
+    collection.push(book);
+    localStorage.setItem('form', JSON.stringify(collection));
+    addingBooks();
+    author.value = '';
+    title.value = '';
+  }
+});
+
+function removeBook(targetId) {
+  const newArr = collection.filter((item) => item.id !== targetId);
+  collection.length = 0;
+  collection.push(...newArr);
+  localStorage.setItem('form', JSON.stringify(collection));
+  addingBooks();
+}
+
+list.addEventListener('click', (e) => {
+  if (e.target.classList.contains('remove')) {
+    const targetid = parseInt(e.target.getAttribute('id'), 10);
+    removeBook(targetid);
+  }
+});
--- refactoring.js
+++ refactoring.js
@@ -9,21 +9,6 @@ class Book {
   }
 }
 
-function addToDisplay(bookObject) {
-  const list = document.querySelector('.list');
-  const book = document.createElement('p');
-  book.classList.add('book');
-  book.setAttribute('id', bookObject.id);
-  book.innerHTML = `${bookObject.title}</span> by ${bookObject.author}`;
-  const removeButton = document.createElement('button');
-  removeButton.innerHTML = 'Remove';
-  removeButton.addEventListener('click', () =>
-    collection.removeBook(bookObject.id)
-  );
-  book.appendChild(removeButton);
-  list.appendChild(book);
-}
-
 class Collection {
   constructor() {
     this.data = [];
@@ -34,15 +19,17 @@ class Collection {
     localStorage.setItem('collection', JSON.stringify(this.data));
     addToDisplay(book);
   }
+
   removeBook(id) {
     const book = document.getElementById(id);
     book.remove();
-    this.data = this.data.filter((bookObj) => bookObj.id !== id);
+    this.data = this.data.filter((bookObject) => bookObject.id !== id);
     localStorage.setItem('collection', JSON.stringify(this.data));
   }
 }
 
 const collection = new Collection();
+
 function getInput() {
   const title = document.querySelector('.title');
   const author = document.querySelector('.author');
@@ -51,6 +38,22 @@ function getInput() {
   author.value = '';
   return book;
 }
+
+// Display Inputs
+function addToDisplay(bookObject) {
+  const list = document.querySelector('.list');
+  const book = document.createElement('p'); 
+  book.classList.add('book');
+  book.setAttribute('id', bookObject.id);
+  book.innerHTML = `${bookObject.title} by ${bookObject.author}`;
+  const removeButton = document.createElement('button');
+  removeButton.classList.add ('remove-button')
+  removeButton.innerHTML = 'Remove';
+  removeButton.addEventListener('click', () => collection.removeBook(bookObject.id));
+  book.appendChild(removeButton);
+  list.appendChild(book);
+}
+
 // Add Button
 const addButton = document.getElementById('add-btn');
 addButton.addEventListener('click', () => {
